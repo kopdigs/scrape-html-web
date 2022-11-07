31658eb0f8391db87a70fae3d7e8c45bbe4ae008
Simone Gattinara <simone.gattinara@smc.it>
2022-11-07 23:03:16 +0100

    second commit:
    -inser .gitignore file
    -fix
    -insert replace and notBeEmpty props

--- test/test.js
+++ test/test.js
@@ -1,15 +1,56 @@
 const scrapeHtmlWeb = require("../lib/scraper");
 
-const options = {
+/*
+  Example 1:
+  - retrieve the list of Node versions from the official site
+*/
+
+const optionsFirstExample = {
   url: "https://nodejs.org/en/blog/",
   mainSelector: ".blog-index",
   childrenSelector: [
-    { key: "date", selector: "time", attr: "", type: "text" },
-    { key: "version", selector: "a", attr: "", type: "text" },
+    { key: "date", selector: "time", type: "text" },
+    { key: "version", selector: "a", type: "text" },
+    { key: "link", selector: "a", attr: "href" },
   ],
 };
 
 (async () => {
-  const data = await scrapeHtmlWeb(options);
-  console.log(data);
+  const data = await scrapeHtmlWeb(optionsFirstExample);
+  console.log("example 1 :", data);
+})();
+
+/*
+  Example 2:
+  - second example that integrates the new replace parameter to be able to manipulate strings in selectors
+*/
+
+const optionsSecondExample = {
+  url: "https://nodejs.org/en/blog/",
+  mainSelector: ".blog-index",
+  childrenSelector: [
+    {
+      key: "date",
+      selector: "time",
+      type: "text",
+      replace: (text) => text + " 2022",
+    },
+    {
+      key: "version",
+      selector: "a",
+      type: "html",
+      replace: /[{()}]/g,
+    },
+    {
+      key: "link",
+      selector: "a",
+      attr: "href",
+    },
+  ],
+};
+
+(async () => {
+  const data = await scrapeHtmlWeb(optionsSecondExample);
+
+  console.log("example 2 :", data);
 })();
