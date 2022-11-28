befb70e47652ee91a05eab30e16c700b25a250cc
Simone Gattinara <simone.gattinara@smc.it>
2022-11-28 11:55:28 +0100

    fix

--- test/scraper-test.js
+++ test/scraper-test.js
@@ -6,27 +6,47 @@ async function getScraping(options) {
 
   const { url, mainSelector, childrenSelector, list } = options || {};
 
-  await axios(`https://scrape-node.onrender.com/scraper?url=${url}`).then(
-    (response) => {
-      const html_data = response.data;
+  await axios(
+    `https://rendertron.onrender.com/render/${encodeURIComponent(url)}`
+  ).then((response) => {
+    const html_data = response.data;
 
-      const $ = cheerio.load(html_data);
+    const $ = cheerio.load(html_data);
 
-      const selectedElem = mainSelector;
+    const selectedElem = mainSelector;
 
-      $(selectedElem)
-        .children()
-        .each((parentIndex, parentElem) => {
-          let obj = {};
+    $(selectedElem)
+      .children()
+      .each((parentIndex, parentElem) => {
+        let obj = {};
 
-          $(childrenSelector).each((index, el) => {
-            const { key, selector, attr, type, canBeEmpty, replace } = el;
+        $(childrenSelector).each((index, el) => {
+          const { key, selector, attr, type, canBeEmpty, replace } = el;
 
-            const value = $(parentElem).find(selector).length
-              ? $(parentElem).find(selector)
-              : $(parentElem);
+          const value = $(parentElem).find(selector).length
+            ? $(parentElem).find(selector)
+            : $(parentElem);
 
-            if (!attr && !type) {
+          if (!attr && !type) {
+            let text = value.text().trim();
+
+            if (replace) {
+              if (replace instanceof RegExp) {
+                text = text.replace(replace, "");
+              } else if (typeof replace === "function") {
+                text = replace(text);
+              }
+            }
+
+            obj = { ...obj, [key]: text };
+          }
+
+          if (attr && attr !== "") {
+            if (value.attr(attr) || canBeEmpty) {
+              obj = { ...obj, [key]: value.attr(attr) };
+            }
+          } else if (type === "text") {
+            if (value.text().trim() || canBeEmpty) {
               let text = value.text().trim();
 
               if (replace) {
@@ -39,52 +59,32 @@ async function getScraping(options) {
 
               obj = { ...obj, [key]: text };
             }
+          } else if (type === "html") {
+            if (value.prop("outerHTML") || canBeEmpty) {
+              let html = value.prop("outerHTML");
 
-            if (attr && attr !== "") {
-              if (value.attr(attr) || canBeEmpty) {
-                obj = { ...obj, [key]: value.attr(attr) };
-              }
-            } else if (type === "text") {
-              if (value.text().trim() || canBeEmpty) {
-                let text = value.text().trim();
-
-                if (replace) {
-                  if (replace instanceof RegExp) {
-                    text = text.replace(replace, "");
-                  } else if (typeof replace === "function") {
-                    text = replace(text);
-                  }
+              if (replace) {
+                if (replace instanceof RegExp) {
+                  html = html.replace(replace, "");
+                } else if (typeof replace === "function") {
+                  html = replace(html);
                 }
-
-                obj = { ...obj, [key]: text };
               }
-            } else if (type === "html") {
-              if (value.prop("outerHTML") || canBeEmpty) {
-                let html = value.prop("outerHTML");
 
-                if (replace) {
-                  if (replace instanceof RegExp) {
-                    html = html.replace(replace, "");
-                  } else if (typeof replace === "function") {
-                    html = replace(html);
-                  }
-                }
-
-                obj = { ...obj, [key]: html };
-              }
+              obj = { ...obj, [key]: html };
             }
-          });
-
-          if (Object.keys(obj).length) {
-            result.push(obj);
-          }
-
-          if (list === false) {
-            return false;
           }
         });
-    }
-  );
+
+        if (Object.keys(obj).length) {
+          result.push(obj);
+        }
+
+        if (list === false) {
+          return false;
+        }
+      });
+  });
 
   try {
     return JSON.stringify(result, null, 2);
